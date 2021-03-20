e78ddb0f6e77f41c241ee7ea9d03e3819ebee246
deepCnapoli <75578508+deepCnapoli@users.noreply.github.com>
2021-03-20 18:29:56 +0100

    Add files via upload

---
+++ src/client/index.js
@@ -0,0 +1,17 @@
+import { checkForName } from './js/nameChecker'
+import { handleSubmit } from './js/app.js'
+import { checkForCity } from './js/checkForCity'
+
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/form.scss'
+import './styles/footer.scss'
+import './styles/header.scss'
+
+//console.log(checkForName);
+
+export {
+    checkForName,
+    handleSubmit,
+    checkForCity
+}
